f15aa57f3750c30c912752bde9346a3319d63165
Andrey Sinelnikov <sinelnikov_a@yahoo.com>
2022-06-15 18:04:54 +0300

    basic fixture with test object

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+# pytest searches for fixtures here
+
+import pytest
+import requests
+
+from configuration import SERVICE_URL
+from src.base_classes.response import Response
+
+@pytest.fixture
+def get_users():
+    response = requests.get(SERVICE_URL)
+    return Response(response)
