2df9145ac428eeb22d71f888e2fabe7f8ac4d8bd
Andrey Sinelnikov <sinelnikov_a@yahoo.com>
2022-06-15 17:33:50 +0300

    pydantic validation and improved assert error log

--- src/base_classes/response.py
+++ src/base_classes/response.py
@@ -5,20 +5,30 @@ class Response:
 
     def __init__(self, response):
         self.response = response
-        self.response_json = response.json()
+        self.response_json = response.json().get("data")  # not meta
         self.response_status = response.status_code
 
     def validate(self, schema):
         if isinstance(self.response_json, list):
             for item in self.response_json:
-                validate(item, schema)
+                schema.parse_obj(item)
         else:
-            validate(self.response_json, schema)
+            schema.parse_obj(self.response_json)
+        return self
+
+    def response_details(self):
+        return f"""
+        Status code: {self.response_status}
+        Requested URL: {self.response.url}
+        Response body: {self.response_json}
+        """
 
     def assert_status_code(self, status_code):
         if isinstance(status_code, list):
-            assert self.response_status in status_code, GlobalErrorMessages.WRONG_STATUS_CODE.value
+            assert self.response_status in status_code, self.response_details()
         else:
-            assert self.response_status == status_code, GlobalErrorMessages.WRONG_STATUS_CODE.value
+            assert self.response_status == status_code, self.response_details()
         return self
+
+
 
--- tests/test_something.py
+++ tests/test_something.py
@@ -1,11 +1,12 @@
 import requests
 from configuration import SERVICE_URL
 from src.base_classes.response import Response
+from src.schemas.user import User
 
-from src.schemas.post import POST_SCHEMA
 
-def test_getting_posts():
-    r = requests.get(url=SERVICE_URL)
-    response = Response(r)
+def test_getting_users_list():
+    response = requests.get(SERVICE_URL)
+    test_object = Response(response)
 
-    response.assert_status_code(200).validate(POST_SCHEMA)
+    test_object.assert_status_code(200).validate(User)
+
